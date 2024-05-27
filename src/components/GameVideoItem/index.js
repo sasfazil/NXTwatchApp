217f9a62111a593d0d6243b6fd4d20d54128b0ee
sasfazil <s.a.s.fazil@gmail.com>
2024-05-27 07:13:13 +0000

    commit message

---
+++ src/components/GameVideoItem/index.js
@@ -0,0 +1,58 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+import VideoContext from '../../context/VideoContext'
+
+import {
+  GameItem,
+  LinkGenerate,
+  ThumbnailContainer,
+  ThumbnailImage,
+  TextContainer,
+  Title,
+  ViewsText,
+  TitleDark,
+  ViewsTextDark,
+} from './styledComponents'
+
+const GameVideoItem = props => {
+  const {game} = props
+  const {id, thumbnailUrl, viewCount, title} = game
+
+  const GameItemDark = () => (
+    <LinkGenerate to={`/videos/${id}`}>
+      <GameItem>
+        <ThumbnailContainer>
+          <ThumbnailImage src={thumbnailUrl} alt="video thumbnail" />
+        </ThumbnailContainer>
+        <TextContainer>
+          <TitleDark>{title}</TitleDark>
+          <ViewsTextDark>{viewCount} Watching Worldwide</ViewsTextDark>
+        </TextContainer>
+      </GameItem>
+    </LinkGenerate>
+  )
+
+  const GameItemLight = () => (
+    <LinkGenerate to={`/videos/${id}`}>
+      <GameItem>
+        <ThumbnailContainer>
+          <ThumbnailImage src={thumbnailUrl} alt="video thumbnail" />
+        </ThumbnailContainer>
+        <TextContainer>
+          <Title>{title}</Title>
+          <ViewsText>{viewCount} Watching Worldwide</ViewsText>
+        </TextContainer>
+      </GameItem>
+    </LinkGenerate>
+  )
+
+  return (
+    <VideoContext.Consumer>
+      {value => {
+        const {isDark} = value
+        return isDark ? GameItemDark() : GameItemLight()
+      }}
+    </VideoContext.Consumer>
+  )
+}
+export default GameVideoItem
